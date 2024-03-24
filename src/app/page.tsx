d7f2db7f700834f1b26554c5b78132e7cad6e2d6
MonsieurMantis <S.Christensen1@ufl.edu>
2024-03-24 00:50:22 -0400

    docx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-'use client';
 import Image from "next/image";
-import {generateDoc} from "./doc-generator";
+import Input from "./input"
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <button className="docuHell" onClick={generateDoc} type="button">ClickMe </button>
-    </main>
-  );
+    return (
+        <main className="flex min-h-screen flex-col items-center p-24">
+            <Input/>
+        </main>
+    );
 }
